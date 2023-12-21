152cbc36b9eedc9629f3a01dd8009f97431ea6e5
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-12-21 23:59:18 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ import { db } from './firebase.config';
 
 function App() {
   const[jobs, setJobs] = useState([]);
+  const[customSearch, setCustomSearch] = useState(false);
 
   const fetchJobs = async() =>{
+    setCustomSearch(false);
     const tempJobs = []
-
     const jobsRef = query(collection(db, "jobs"));
     const q = query(jobsRef, orderBy("postedOn", "desc"));
     const req = await getDocs(q);
@@ -29,12 +30,14 @@ function App() {
     setJobs(tempJobs);
   }
 
-  const fetchJobsCustom = async(jobCriteria) =>{
+  
+  const fetchJobsCustom = async(jobCriteria) => {
+    setCustomSearch(true);
     const tempJobs = []
-
     const jobsRef = query(collection(db, "jobs"));
-    const q = query(jobsRef, where("type", "==", jobCriteria.type), where("title", "==", jobCriteria.title), where("location", "==", jobCriteria.location), where("experience", "==", jobCriteria.experience) ,orderBy("postedOn", "desc"));
+    const q = query(jobsRef, where("type", "==", jobCriteria.type), where("title", "==", jobCriteria.title), where("experience", "==", jobCriteria.experience), where("location", "==", jobCriteria.location) ,orderBy("postedOn", "desc"));
     const req = await getDocs(q);
+    
     req.forEach((job) => {
       
       //console.log(doc.id, " => ", doc.data());
@@ -55,6 +58,10 @@ function App() {
       <Navbar />
       <Header />
       <SearchBar fetchJobsCustom={fetchJobsCustom}/>
+      {customSearch && 
+      <button onClick={fetchJobs} className='flex pl-[1250px] mb-2'>
+        <p className='bg-blue-500 px-10 py-2 rounded-md text-white'>Clear Filters</p>
+      </button>}
       {jobs.map((job)=>(
         <JobCard key={job.id} {...job}/>
       ))}
